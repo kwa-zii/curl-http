028c30ca27a2b4c8774eab8e2b0f0714d884cfc5
Kevin <qinqiwei@hotmail.com>
2017-10-19 19:50:02 +0800

    增加header头定义

--- tests/test-get.php
+++ tests/test-get.php
@@ -10,6 +10,7 @@ require_once 'bootstrap.php';
 use Qinqw\Curl\Http\Request;
 
 $req = new Request("");
+$headers = array('Content-type: application/json','X-host: etcp.cn');
 $req->get("http://www.baidu.com");
 $res = $req->exec();
 var_dump($res);
