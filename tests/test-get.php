61b0fd2a07dca92d7a8f5248ccd00c0861d4c6a9
Kevin <qinqiwei@hotmail.com>
2017-10-20 11:33:29 +0800

    update comment

--- tests/test-get.php
+++ tests/test-get.php
@@ -1,9 +1,15 @@
 <?php
 /**
+ * One of the curl - based HTTP libraries implemented in PHP
  * Test http request
  * 
- * @author Kevin <qinqiwei@hotmail.com>
+ * @category Crul_Http
+ * @package  Qinqw\Curl\Http
+ * @author   Kevin <qinqiwei@hotmail.com>
+ * @license  Apache License V2
+ * @link     https://github.com/qinqw/curl-http
  */
+
 namespace tests;
 require_once 'bootstrap.php';
 
--- tests/test-headers.php
+++ tests/test-headers.php
@@ -1,8 +1,13 @@
 <?php
 /**
+ * One of the curl - based HTTP libraries implemented in PHP
  * Test http request
  * 
- * @author Kevin <qinqiwei@hotmail.com>
+ * @category Crul_Http
+ * @package  Qinqw\Curl\Http
+ * @author   Kevin <qinqiwei@hotmail.com>
+ * @license  Apache License V2
+ * @link     https://github.com/qinqw/curl-http
  */
 namespace tests;
 require_once 'bootstrap.php';
